836288ffb560ca72ad9ac8bebf5f34da9a7dfc10
Sean Cafe <seanamir30@gmail.com>
2021-11-14 13:56:05 +0800

    added blog app

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
     def index
-        @article = Article.all
+        @article = Article.all.order('created_at desc')
     end
         
     def new
@@ -18,15 +18,24 @@ class ArticlesController < ApplicationController
     end
         
     def edit
-
+        @article = Article.find(params[:id])
     end
         
     def update
+        @article = Article.find(params[:id])
 
+        if @article.update(article_params)
+            redirect_to articles_path
+        else
+            render :edit
+        end
     end
         
     def delete
+        @article = Article.find(params[:id])
+        @article.destroy
 
+        redirect_to articles_path
     end
 
     private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get '/articles' => 'articles#index'
   get '/articles/new' => 'articles#new', as: 'new_article'
-  post 'articles' => 'articles#create', as: 'create_article'
+  post '/articles' => 'articles#create', as: 'create_article'
+  get '/articles/:id/edit' => 'articles#edit', as: 'edit_article'
+  patch '/articles/:id' => 'articles#update', as: 'update_article'
+  delete '/articles/:id/delete' => 'articles#delete', as: 'delete_article'
 end
