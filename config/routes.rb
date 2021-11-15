07b9eadc0c31254b9f12d3f52f28c31be61d8366
Sean Cafe <seanamir30@gmail.com>
2021-11-15 09:17:51 +0800

    removed rails yay page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/' => 'articles#index'
   get '/articles' => 'articles#index'
   get '/articles/new' => 'articles#new', as: 'new_article'
   post '/articles' => 'articles#create', as: 'create_article'
